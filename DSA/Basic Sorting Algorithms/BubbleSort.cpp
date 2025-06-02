9b21e061e02ba451309ceeaaf9f5e93f922b7f90
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-06-02 20:28:23 +0530

    new file:   DSA/Basic Sorting Algorithms/BubbleSort.cpp 	new file:   DSA/Basic Sorting Algorithms/BubbleSort.exe

---
+++ DSA/Basic Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void PrintArray(int *arr, int n){
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<", ";
+    }
+}
+
+void BubbleSort(int *arr, int n){
+    for (int i = 0; i <n-1; i++){
+        for(int j = 0; j<n-i-1; j++){
+            if(arr[j]> arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    PrintArray(arr, n);
+}
+
+int main(){
+    int arr[] = {5, 4, 1, 2, 3};
+    int n = sizeof(arr)/sizeof(int);
+
+    BubbleSort(arr, n);
+
+    return 0;
+}
