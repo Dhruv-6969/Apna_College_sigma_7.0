fa5e89391c587b295f6366e512030af6adb089f7
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-06-02 20:40:00 +0530

    modified:   DSA/Basic Sorting Algorithms/BubbleSort.cpp 	modified:   DSA/Basic Sorting Algorithms/BubbleSort.exe

--- DSA/Basic Sorting Algorithms/BubbleSort.cpp
+++ DSA/Basic Sorting Algorithms/BubbleSort.cpp
@@ -8,11 +8,17 @@ void PrintArray(int *arr, int n){
 }
 
 void BubbleSort(int *arr, int n){
+    bool isSwap = false;
     for (int i = 0; i <n-1; i++){
         for(int j = 0; j<n-i-1; j++){
             if(arr[j]> arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+        if(!isSwap){
+            //array is already sorted
+            return;
         }
     }
     PrintArray(arr, n);
