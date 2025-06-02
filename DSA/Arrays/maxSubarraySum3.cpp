47955f32e928ee26e92cd18ec29803e2c4d6989c
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-06-02 11:11:02 +0530

    modified:   DSA/Arrays/maxSubarraySum3.cpp 	new file:   DSA/Arrays/maxSubarraySum3.exe

--- DSA/Arrays/maxSubarraySum3.cpp
+++ DSA/Arrays/maxSubarraySum3.cpp
@@ -1,1 +1,25 @@
 //Kadane's Algorithm
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void SubarraySum3(int *arr, int n){
+    int maxSum = INT_MIN;
+    int curSum = 0;
+    for(int i = 0; i<n; i++){
+        curSum += arr[i];
+        maxSum = max(maxSum, curSum);
+        if(curSum<0){
+            curSum = 0;
+        }
+    }
+    cout<< "Maximum Sum of Subarray = "<< maxSum;
+}
+
+int main(){
+    int arr[] = {2, -3, 6, -5, 4, 2};
+    int n = sizeof(arr)/sizeof(int);
+
+    SubarraySum3(arr, n);
+    return 0;
+}
