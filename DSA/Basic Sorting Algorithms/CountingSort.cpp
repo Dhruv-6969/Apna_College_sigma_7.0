678eba24382828dc5484e5c07ea51f3bad66ae3e
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-06-23 15:15:28 +0530

    new file:   DSA/Basic Sorting Algorithms/CountingSort.cpp
    	modified:   DSA/Basic Sorting Algorithms/CountingSort.cpp
    
    	DSA/Basic Sorting Algorithms/CountingSort.exe

---
+++ DSA/Basic Sorting Algorithms/CountingSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void CountSort(int *arr, int n){
+    for(int i = 0; i<n ; i++){
+        
+    } 
+}
+
+int main(){
+    int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+    int n = sizeof(arr)/sizeof(int);
+
+    return 0;
+}
