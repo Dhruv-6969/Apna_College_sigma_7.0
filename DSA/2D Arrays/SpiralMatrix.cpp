94c0f826970bd26079df939c22ae60e0cf2c325a
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-06-23 19:51:12 +0530

    new file:   DSA/2D Arrays/SpiralMatrix.cpp 	new file:   DSA/2D Arrays/SpiralMatrix.exe

---
+++ DSA/2D Arrays/SpiralMatrix.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void spiralMatrix(int mat[][4], int n, int m)
+{
+    int srow = 0, erow = n - 1;
+    int scol = 0, ecol = m - 1;
+
+    while (srow <= erow && scol <= ecol)
+    {
+        // top
+        for (int i = scol; i <= ecol; i++)
+        {
+            cout << mat[srow][i] << " ";
+        }
+        srow++;
+
+        // right
+        for (int i = srow; i <= erow; i++)
+        {
+            cout << mat[i][ecol] << " ";
+        }
+        ecol--;
+
+        // bottom
+        for (int i = ecol; i >= scol; i--)
+        {
+            cout << mat[erow][i] << " ";
+        }
+        erow--;
+
+        // left
+        for (int i = erow; i >= srow; i--)
+        {
+            cout << mat[i][scol] << " ";
+        }
+        scol++;
+    }
+}
+
+int main()
+{
+    int mat[4][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+
+    spiralMatrix(mat, 4, 4);
+    return 0;
+}
