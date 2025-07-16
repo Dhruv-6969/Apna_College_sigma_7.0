f28b37db12cbd835a35c6bce6a10a02141cd0b58
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-07-16 19:48:31 +0530

    modified:   DSA/Linked List/LL_Basics.cpp 	modified:   DSA/Linked List/LL_Basics.exe

--- DSA/Linked List/LL_Basics.cpp
+++ DSA/Linked List/LL_Basics.cpp
@@ -10,6 +10,13 @@ public:
         data = val;
         next = NULL;
     }
+
+    ~Node(){
+        if(next != NULL){
+            delete next;
+            next = NULL;
+        }
+    }
 };
 
 class List{
@@ -20,6 +27,13 @@ public:
     List(){
         head = NULL;
         tail = NULL;
+    }
+
+    ~List(){
+        if(head!= NULL){
+            delete head;
+            head = NULL;
+        }
     }
 
     void push_front(int val){
@@ -53,6 +67,7 @@ public:
             cout<< temp->data << " ";
             temp = temp->next;
         }
+        cout << endl;
     }
 
     void insert(int val, int pos){
@@ -61,13 +76,55 @@ public:
 
         for(int i = 0; i<pos-1; i++){
             if(temp == NULL){
-                cout << "position is invalid";
+                cout << "position is invalid\n";
             }
             temp = temp->next;
         }
 
         newNode->next = temp->next;
         temp->next = newNode;
+    }
+
+    void pop_front(){
+        if(head == NULL){
+            cout<< "Linked List is empty\n";
+        }
+
+        Node* temp = head;
+
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void pop_back(){
+        if(head == NULL){
+            cout<< "Linked List is empty\n";
+        }
+
+        Node * temp = head;
+        
+        while(temp->next != tail){
+            temp = temp->next;
+        }
+
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+    }
+
+    int searchItr(int key){
+        Node * temp = head;
+        int idx = 0;
+
+        while(temp!= NULL){
+            if(temp->data == key){
+                return idx;
+            }
+            temp = temp->next;
+            idx++;
+        }
+        return -1;
     }
 };
 
@@ -77,13 +134,20 @@ int main(){
     ll.push_front(3);
     ll.push_front(2);
     ll.push_front(1);
+    ll.push_front(0);
 
     ll.push_back(5);
     ll.push_back(6);
 
-    ll.insert(4, 3);
+    ll.insert(4, 4);
+
+    ll.pop_front();
+
+    ll.pop_back();
 
     ll.printList();
+
+    cout << ll.searchItr(3) << endl;
 
     return 0;
 }
