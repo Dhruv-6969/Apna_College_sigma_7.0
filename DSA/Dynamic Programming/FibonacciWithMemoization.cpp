ab24b7552c4d96a56c9f8daa0975db995582c775
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2026-06-30 13:59:33 +0530

    renamed:    DSA/Dynamic Programming/Fibonacci.cpp -> DSA/Dynamic Programming/FibonacciWithMemoization.cpp 	renamed:    DSA/Dynamic Programming/Fibonacci.exe -> DSA/Dynamic Programming/FibonacciWithMemoization.exe

--- DSA/Dynamic Programming/FibonacciWithMemoization.cpp
+++ DSA/Dynamic Programming/FibonacciWithMemoization.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int fibDP(int n, vector <int> &f){
-    if(n == 0 || n == 1){
+    if(n <= 1){
         return n;
     }
 
-    if(n != -1){
+    if(f[n] != -1){
         return f[n];
     }
 
@@ -19,4 +19,5 @@ int main(){
     vector <int> f(n+1, -1);
 
     cout << fibDP(n, f) << endl;
+    return 0;
 }
