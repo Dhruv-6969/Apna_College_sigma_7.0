d0d1bfd3a7f224d93bf974b64f6b7da5acab40c5
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2026-07-03 03:02:40 +0530

    new file:   DSA/Dynamic Programming/KnapsackWithRecursion.cpp 	new file:   DSA/Dynamic Programming/KnapsackWithRecursion.exe

---
+++ DSA/Dynamic Programming/KnapsackWithRecursion.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapsackRec(vector <int> val, vector <int> wt, int W, int n){ //O(2^n)
+    if( W == 0 || n == 0){
+        return 0;
+    }
+    
+    int itemWt = wt[n-1];
+    int itemVal = val[n-1];
+
+    if(itemWt <= W){
+        //include
+        int ans1 = knapsackRec(val, wt, W-itemWt, n-1) + itemVal;
+
+        //exclude
+        int ans2 = knapsackRec(val, wt, W, n-1);
+
+        return max(ans1, ans2);
+    }
+    else{
+        //exclude
+        return knapsackRec(val, wt, W, n-1);
+    }
+}
+
+int main(){
+    vector <int> val = {15, 14, 10, 45, 30};
+    vector <int> wt = {2, 5, 1, 3, 4};
+    int W = 7;
+    int n = 5;
+
+    cout << knapsackRec(val, wt, W, n);
+
+    return 0;
+}
