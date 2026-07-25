7547f244d9bdab290f31c14bcaaa98605ed7550d
Dhruv-6969 <dhrubakshay@gmail.com>
2026-07-25 08:19:06 +0530

    modified:   DSA/Dynamic Programming/KnapsackWithTabulation.cpp 	modified:   DSA/Dynamic Programming/KnapsackWithTabulation.exe

--- DSA/Dynamic Programming/KnapsackWithTabulation.cpp
+++ DSA/Dynamic Programming/KnapsackWithTabulation.cpp
@@ -1,41 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int knapsackTab(vector<int> val, vector<int> wt, int W, int n, vector<vector<int>> &dp){
-    if(n == 0 || W == 0){
-        return 0;
-    }
-
-    int itemVal = val[n-1];
-    int itemWt = wt[n-1];
-
-    if(dp[n][W] != -1){
-        return dp[n][W];
-    }
-
-    if(itemWt <= W){
-        //include
-        int ans1 = knapsackTab(val, wt, W-itemWt, n-1, dp) + itemVal;
-        //exclude
-        int ans2 = knapsackTab(val, wt, W, n-1, dp);
-        
-        dp[n][W] = max(ans1, ans2);
-    }
-    else{
-        //exclude
-        dp[n][W] = knapsackTab(val, wt, W, n-1, dp);
-    }
-    return dp[n][W];
-}
-
 int main(){
     vector <int> val = {15, 14, 10, 45, 30};
     vector <int> wt = {2, 5, 1, 3, 4};
     int W = 7;
     int n = 5;
 
-    vector<vector<int>> dp(n+1, vector<int> (W+1, -1));
-    cout << knapsackTab(val, wt, W, n, dp);
+    vector<vector<int>> dp(n+1, vector<int> (W+1, 0));
+
+    for(int i = 1; i<n+1; i++){
+        for(int j = 1; j<W+1; j++){
+            int itemWt = wt[i-1];
+            int itemVal = val[i-1];
+
+            if(itemWt <= j){
+                dp[i][j] = (max(itemVal + dp[i-1][j - itemWt], dp[i-1][j]));
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    cout << dp[n][W];
 
     return 0;
 }
